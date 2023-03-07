a4b7355d1400e1cccfc5ddfcd2ea8e020bca69b1
Kermo <kermo.ohno@voco.com>
2023-03-07 13:43:36 +0200

    Created from request responce with API data

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -25,6 +28,24 @@ app.get('/', function (req, res){
             })
         })
 
+})
+
+app.post('/', function (req, res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index.ejs', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 })
 
 app.listen(3003)
